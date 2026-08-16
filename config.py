132ea6a4da2772ea00ad9dd6f736f791aff6e739
cyberamit07 <cyberamit07@gmail.com>
2026-08-17 00:20:18 +0530

    Replace environment variables with hardcoded values

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
+BOT_TOKEN = os.getenv("8877929696:AAHr9UhWW_iTCJboHGCqgsuERJYotuq23Nc", "").strip()
 
 try:
-    ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+    ADMIN_ID = int(os.getenv("8603893462", "0"))
 except ValueError:
     ADMIN_ID = 0
 
